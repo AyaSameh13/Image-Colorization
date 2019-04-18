92b47bb088b48bec3f1b0c16454aba209b9df03a
Aya <ayasameh1313@gmail.com>
2019-04-18 07:40:54 +0200

    changes in data downloader & building model

--- data_downloader/download.sh
+++ data_downloader/download.sh
@@ -8,7 +8,7 @@ do
 	url=$(echo "$line" | tr -d '"')
 	file_name=$(basename $line)
 	file_name=$(echo "$file_name" | tr -d '"')
-	wget -q $url -O $download_dir'/'$file_name --tries=1 --waitretry=0 --read-timeout=3
+	wget -q $url -O $download_dir'/'$file_name  -T  1 -t 1 -nc -c
 	size=$(wc -c $download_dir'/'$file_name | awk '{print $1}')
 	if  (($size <= 0)); then
 	    	rm $download_dir'/'$file_name
--- data_downloader/txt_to_csv.py
+++ data_downloader/txt_to_csv.py
@@ -3,17 +3,21 @@ import csv
 
 txt_path = ""
 csv_path = ""
+max_download = 0
 
 def extract_url():
     global txt_path
     global csv_path
+    global max_download
+    i = 0
     lines = open(txt_path, "r")
     csv_file = open(csv_path, "w")
     out = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
     urls = []
     for line in lines:
         urls.append([line.split()[1]])
-    print(urls)
+        i = i + 1
+        if i > max_download: break
     out.writerows(urls)
     csv_file.close()
 
@@ -21,12 +25,15 @@ def extract_url():
 def main():
     global txt_path
     global csv_path
+    global max_download
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', help="path to text url file", action='store', dest="txt_path", default="urls.txt")
     parser.add_argument('-c', help="path to csv url file", action='store', dest="csv_path", default="urls.csv")
+    parser.add_argument('-m', help="max num of urls", action='store', dest="max", default="urls.csv")
     args = parser.parse_args()
     txt_path = args.txt_path
     csv_path = args.csv_path
+    max_download = int(args.max)
     extract_url()
 
 
